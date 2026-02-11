292af0509b4da81fcf53bb4751590a3a56568d56
Srivignesh M <vignesh.m1683@gmail.com>
2026-02-11 22:42:46 +0530

    chore: Update API base URL from localhost to Vercel deployment.

--- frontend/src/api/projectApi.js
+++ frontend/src/api/projectApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://rudratek-fullstack-task.vercel.app',
 });
 
 export const getProjects = async (params) => {
